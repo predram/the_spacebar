d6e2e5124cf0fe28465a155124b51c0592401044
Predrag <predram@bitbucket.ort>
2018-06-21 03:50:14 +0200

    proba

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+	/**
+	*@Route("/")
+	*/
+	public function homepage(){
+		return new Response('OMG? My first page.... WOW');
+	}
+
+	/**
+	*@Route("/news/{slug}")
+	*/
+	public function show($slug){
+		return new Response(sprintf('Future page to show space article:"%s"', $slug));
+	}
+} 
