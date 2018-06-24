58c374d55b2c17db21de0a1d058bccbce107aa2e
predram <predram@github.com>
2018-06-24 09:55:39 +0200

    regularno...

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 	/**
 	*@Route("/")
@@ -17,6 +18,16 @@ class ArticleController
 	*@Route("/news/{slug}")
 	*/
 	public function show($slug){
-		return new Response(sprintf('Future page to show space article:"%s"', $slug));
+		/*return new Response(sprintf('Future page to show space article:"%s"', $slug));
+	}*/
+	$comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+		];
+	return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),'comments'=>$comments,
+		]);
+
 	}
 } 
