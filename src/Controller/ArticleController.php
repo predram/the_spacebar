0a8bffc0a4bce8c143d95b1dd83842ea183e611a
predram <predram@github.com>
2018-06-25 22:25:44 +0200

    dalje

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,18 +4,20 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ArticleController extends AbstractController
 {
 	/**
-	*@Route("/")
+	*@Route("/", name="app_homepage")
 	*/
 	public function homepage(){
-		return new Response('OMG? My first page.... WOW');
+//		return new Response('OMG? My first page.... WOW');
+        return $this->render('article/homepage.html.twig');
 	}
 
 	/**
-	*@Route("/news/{slug}")
+	*@Route("/news/{slug}", name="article_show")
 	*/
 	public function show($slug){
 		/*return new Response(sprintf('Future page to show space article:"%s"', $slug));
@@ -25,9 +27,19 @@ class ArticleController extends AbstractController
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
 		];
+	    dump($slug, $this);
 	return $this->render('article/show.html.twig', [
-            'title' => ucwords(str_replace('-', ' ', $slug)),'comments'=>$comments,
+            'title' => ucwords(str_replace('-', ' ', $slug)),'slug'=>$slug,'comments'=>$comments,
 		]);
 
 	}
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+	public function toggleArticleHeart($slug){
+        //todo
+
+        return new JsonResponse(['hearts'=>rand(5,100)]);
+    }
 } 
